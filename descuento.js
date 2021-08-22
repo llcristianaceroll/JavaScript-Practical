414b0115b4d2aabd0329233271a3cbd5d0d5a61d
Cristian <llcristianaceroll@gmail.com>
2021-08-22 14:09:48 -0500

    Se agrego promoción de cupones usando array.find() para validar si el cúpon es valido

--- descuento.js
+++ descuento.js
@@ -69,7 +69,7 @@ function onClickButtonPriceDiscount(){
 // });
 
 //Solucion 2
-
+/*
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 -  descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
@@ -107,8 +107,54 @@ function onClickButtonPriceDiscount(){
         const resultP = document.getElementById('ResultP');
         resultP.innerText = 'El precio con cupón es: $' + precioConDescuento;
     }
+*/
+
+//Solucion 3
 
 
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 -  descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento;
+}
+const coupons = [
+    {
+        name: "JuanDC_es_Batman",
+        discount: 15,
+    },
+    {
+        name: "pero_no_le_digas_a_nadie",
+        discount: 30,
+    },
+    {
+        name: "es_un_secreto",
+        discount: 25,
+    },
+];
 
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById('InputPrice');
+    const priceValue = inputPrice.value;
+    
+    const inputCoupon = document.getElementById('InputCoupon');
+    const couponValue = inputCoupon.value;
 
+    const isCouponValueValid = (coupon) => {
+    return coupon.name === couponValue;
+};
 
+const userCoupon = coupons.find(isCouponValueValid);
+
+if (!userCoupon) {
+    alert("El cupón " + couponValue + "no es válido");
+} else {
+       
+        const discount = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, discount);
+
+        const resultP = document.getElementById("ResultP");
+        resultP.innerText = "El precio con descuento con el cupon " + couponValue + " son: $" + precioConDescuento;
+}
+
+}
+ 
