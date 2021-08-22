d91d42b6478aa413b9c9fb82d0c04b8a52557c67
Cristian <llcristianaceroll@gmail.com>
2021-08-21 19:33:18 -0500

    funciones

--- descuento.js
+++ descuento.js
@@ -1,1 +1,17 @@
-console.log('Hola')
+// const precioOriginal = 120;
+// const descuento = 18;
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 -  descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento;
+}
+
+
+
+// console.log({
+//     precioOriginal,
+//     descuento,
+//     porcentajePrecioConDescuento,
+//     precioConDescuento
+// });
