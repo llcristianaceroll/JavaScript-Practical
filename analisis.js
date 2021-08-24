0d667b71435022bc61b76c6b66c249219aa6ff7c
Cristian <llcristianaceroll@gmail.com>
2021-08-24 08:47:25 -0500

    Calculo top 10 salarios usando splice()

--- analisis.js
+++ analisis.js
@@ -1,14 +1,4 @@
-const salariesCol = colombia.map(
-    function (person) {
-        return person.salary;
-    }
-);
-
-const salariesColSorted = salariesCol.sort(
-    function (salaryA, SalaryB) {
-        return salaryA - SalaryB;
-    }
-);
+//Helpers 
 
 function esPar(num) {
     return (num % 2 === 0);
@@ -27,6 +17,9 @@ const promedioLista = sumaLista / lista.length;
 return promedioLista;
 }
 
+// Calculadora de medianas
+
+
 function medianaSalaries(list) {
     const mitad = parseInt(list.length / 2);    
 
@@ -42,4 +35,32 @@ function medianaSalaries(list) {
     }
 }
 
-console.log(medianaSalaries(salariesColSorted));
+// Mediana General
+const salariesCol = colombia.map(
+    function (person) {
+        return person.salary;
+    }
+);
+
+const salariesColSorted = salariesCol.sort(
+    function (salaryA, SalaryB) {
+        return salaryA - SalaryB;
+    }
+);
+
+const medianaGeneralCol = medianaSalaries(salariesColSorted);
+
+// Mediana del top 10%
+
+
+const spliceStart = parseInt((salariesColSorted.length * 90) / 100);
+const spliceCount = parseInt(salariesColSorted.length - spliceStart);
+
+const salariesColTop10 = salariesColSorted.splice(spliceStart, spliceCount);
+
+const medianaTop10Col = medianaSalaries(salariesColTop10);
+
+console.log({
+    medianaGeneralCol,
+    medianaTop10Col,
+});
